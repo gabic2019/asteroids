761dcb98739fbabac3a10883f5ca20c68d81a587
gabic2019 <gabi@gabrielcojocaru.com>
2024-10-29 23:33:10 +0200

    Adding player and hitbox

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 # Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
@@ -30,12 +31,14 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 , 0)
 
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
        pygame.Surface.fill(screen, (0,0,0))
+       player.draw(screen) 
        pygame.display.flip()
        dt = clock.tick(60) / 1000
 
