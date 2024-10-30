6aebb92437eb3df8d152d8966863aa9747c15294
gabic2019 <gabi@gabrielcojocaru.com>
2024-10-30 22:23:36 +0200

    let's start moving

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
 import pygame
 from constants import *
 from player import Player
-
-# Base class for game objects
-class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius):
-        # we will be using this later
-        if hasattr(self, "containers"):
-            super().__init__(self.containers)
-        else:
-            super().__init__()
-
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
-
-    def draw(self, screen):
-        # sub-classes must override
-        pass
-
-    def update(self, dt):
-        # sub-classes must override
-        pass
+from circleshape import *
 
 def main():
     print("Starting asteroids!")
@@ -38,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
        pygame.Surface.fill(screen, (0,0,0))
+       player.update(dt)
        player.draw(screen) 
        pygame.display.flip()
        dt = clock.tick(60) / 1000
