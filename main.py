95ed1620d0217e861279131e91419126e0df24fb
gabic2019 <gabi@gabrielcojocaru.com>
2024-11-01 21:13:26 +0200

    adding groups of objects

--- main.py
+++ main.py
@@ -11,15 +11,30 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 , 0)
+    
+
+    updatable  = pygame.sprite.Group() 
+    drawable  = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 , 0) #init after adding to containers
 
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+       
        pygame.Surface.fill(screen, (0,0,0))
-       player.update(dt)
-       player.draw(screen) 
+       
+       for to_update in updatable:
+           to_update.update(dt)
+
+       for to_draw in drawable:
+           to_draw.draw(screen)
+
+       #player.update(dt)
+       #player.draw(screen) 
        pygame.display.flip()
        dt = clock.tick(60) / 1000
 
